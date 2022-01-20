c527e0384ddd09a62f2c6e5cc4f1c609e398a71b
tepis <admin@stinum.ru>
2022-01-20 15:09:26 +0300

    4.4 Appbar text

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -11,8 +11,18 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello Scaffold'),
+      appBar: AppBar(
+        toolbarHeight: 452,
+        backgroundColor: Colors.white,
+        shadowColor: Colors.transparent,
+        centerTitle: false,
+        title: Container(
+          padding: EdgeInsets.fromLTRB(16, 40, 16, 322),
+            child: Text('Список\nинтересных мест',
+                style: TextStyle(
+                    color: Color(0xFF252849),
+                    fontSize: 32,
+                    fontFamily: 'Roboto', fontWeight: FontWeight.bold),),),
       ),
     );
   }
