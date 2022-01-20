04fa4b22bdf2270374b944e9491c254b06ed987a
tepis <admin@stinum.ru>
2022-01-20 16:29:37 +0300

    4.4 RichText

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -18,11 +18,35 @@ class _SightListScreenState extends State<SightListScreen> {
         centerTitle: false,
         title: Container(
           padding: EdgeInsets.fromLTRB(16, 40, 16, 322),
-            child: Text('Список\nинтересных мест',
-                style: TextStyle(
-                    color: Color(0xFF252849),
-                    fontSize: 32,
-                    fontFamily: 'Roboto', fontWeight: FontWeight.bold),),),
+          child: RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "С",
+                  style: TextStyle(color: Color(0xff4CAF50)),
+                ),
+                TextSpan(
+                  text: "писок",
+                ),
+                TextSpan(
+                  text: "\nи",
+                  style: TextStyle(
+                    color: Color(0xfffcdd3d),
+                  ),
+                ),
+                TextSpan(
+                  text: 'нтересных мест',
+                ),
+              ],
+              style: TextStyle(
+                color: Color(0xff252849),
+                fontWeight: FontWeight.bold,
+                fontSize: 32,
+                fontFamily: 'Roboto',
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
