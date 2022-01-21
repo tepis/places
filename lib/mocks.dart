717adfb5b7ac2befc5ad631220c9ef50f739f358
tepis <admin@stinum.ru>
2022-01-21 14:36:36 +0300

    4.6.1 Mocks

---
+++ lib/mocks.dart
@@ -0,0 +1,25 @@
+import 'package:places/domain/sight.dart';
+
+final List<Sight> mocks = [
+  Sight(
+      name: 'Домбай',
+      lat: 43.2916700,
+      lon: 41.6216700,
+      url: 'http://www.dombaj.ru/',
+      details: 'Популярный российский горнолыжный курорт, расположенный на Северном Кавказе',
+      type: 'курорт',),
+  Sight(
+      name: 'Чегет',
+      lat: 43.2452965,
+      lon: 42.5233803,
+      url: 'https://www.vpoxod.ru/page/toponym/goracheget_info',
+      details: 'неофициальное устоявшееся название горы Кавказа',
+      type: 'гора',),
+  Sight(
+      name: 'Озеро Любви',
+      lat: 43.536667,
+      lon: 41.338056,
+      url: 'http://www.dombaj.ru/',
+      details: 'Популярный российский горнолыжный курорт, расположенный на Северном Кавказе',
+      type: 'курорт',),
+];
