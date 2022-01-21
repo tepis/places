4d6474bee14167eb82009a143cadf537e08b4f85
tepis <admin@stinum.ru>
2022-01-21 14:55:23 +0300

    4.6.2 Sight card

---
+++ lib/ui/screen/sight_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:places/domain/sight.dart';
+
+class SightCard extends StatelessWidget {
+  final Sight sight;
+  const SightCard(this.sight, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+    );
+  }
+}
