29c6c2a84b63f8a82a3dac4f57daeef7100e8f07
tepis <admin@stinum.ru>
2022-01-27 12:03:35 +0300

    4.8 AspectRatio

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -7,89 +7,92 @@ class SightCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Stack(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 96,
-              decoration: const BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(16.0),
-                  topRight: Radius.circular(16.0),
-                ),
-              ),
-            ),
-            Positioned(
-              top: 16,
-              left: 16,
-              child: Text(
-                sight.type,
-                style: const TextStyle(
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.w700,
-                  fontSize: 14,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 19,
-              right: 19,
-              child: Container(
-                width: 20.0,
-                height: 18.0,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
-        ),
-        Container(
-          width: double.infinity,
-          padding: EdgeInsets.all(16),
-          height: 96,
-          decoration: const BoxDecoration(
-            color: Color(0xFFF5F5F5),
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(16.0),
-              bottomRight: Radius.circular(16.0),
-            ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return AspectRatio(
+      aspectRatio: 3 / 2,
+      child: Column(
+        children: [
+          Stack(
             children: [
-              Text(
-                sight.name,
-                style: const TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 16,
-                  color: Color(0xff3B3E5B),
-                  fontWeight: FontWeight.w500,
+              Container(
+                width: double.infinity,
+                height: 96,
+                decoration: const BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0),
+                  ),
                 ),
               ),
-              Container(
-                margin: EdgeInsets.only(top: 2),
-                width: double.infinity,
-                height: 18,
+              Positioned(
+                top: 16,
+                left: 16,
                 child: Text(
-                  sight.details,
-                  textAlign: TextAlign.left,
-                  overflow: TextOverflow.ellipsis,
+                  sight.type,
                   style: const TextStyle(
-                      fontSize: 14,
-                      color: Color(0xff3B3E5B),
-                      fontFamily: 'Roboto',),
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w700,
+                    fontSize: 14,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 19,
+                right: 19,
+                child: Container(
+                  width: 20.0,
+                  height: 18.0,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ],
           ),
-        ),
-      ],
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(16),
+            height: 96,
+            decoration: const BoxDecoration(
+              color: Color(0xFFF5F5F5),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(16.0),
+                bottomRight: Radius.circular(16.0),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  sight.name,
+                  style: const TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 16,
+                    color: Color(0xff3B3E5B),
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 2),
+                  width: double.infinity,
+                  height: 18,
+                  child: Text(
+                    sight.details,
+                    textAlign: TextAlign.left,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                        fontSize: 14,
+                        color: Color(0xff3B3E5B),
+                        fontFamily: 'Roboto',),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
